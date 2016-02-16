0eea06b891e6a14f54d1d081220190c27b91bf10
shawn <shawn.xue@otms.cn>
2016-02-16 16:22:32 +0800

    fix bug

--- js/views/cars.operatorpage.js
+++ js/views/cars.operatorpage.js
@@ -23,7 +23,7 @@ class FeedComponent extends React.Component {
     }
 
     getCarDescription() {
-        var des = " " + Cars.brands[this.props.item.car.brand].name + " " + this.props.item.car.series + " " + this.props.item.car.displacement + " " + this.props.item.car.year;
+        var des = " " + this.props.item.car.brand.name + " " + this.props.item.car.series + " " + this.props.item.car.displacement + " " + this.props.item.car.year;
         return (
             <span className="feed-title">
                 <Image className="feed-car-brand"
